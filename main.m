2bcca2602b314cc6fa86efd37fed4a91547a9c00
Tinkle Chugh <tinkle.chugh@gmail.com>
2024-07-11 16:41:52 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -1,40 +1,34 @@
+close all; clc; clear;
+
+%% test 1
 PF = [1,10;5,5;8,2;10,1];
 r = [11,11];
 
 mu = [4,4];
 sigma = [0.5,0.3];
 
-EHV1 = EHVI_2d(PF,r,mu,sigma)
+EHVI = EHVI_2d(PF,r,mu,sigma)
 
-EHV1 = EHVI_monte_carlo(PF,r,mu,sigma);
+EHVI_monte_carlo = EHVI_monte_carlo(PF,r,mu,sigma)
 
-%%
+%% Test 2
 PF = dlmread('DTLZ2.2D.pf');
 
-%%
 data = readtable('DTLZ2.2D.2.csv');
 y1 = data.Var1;
 y2 = data.Var2;
 
-y1 = str2double(y1);
-y2 = str2double(y2);
 y1(1) = []; y2(1) = []; 
 
 
-%%
 s1 = data.Var3;
 s2 = data.Var3;
-s1 = str2double(s1);
-s2 = str2double(s2);
+
 s1(1) = []; s2(1) = []; 
 
 
-%%
 r1 = data.Var5;
 r2 = data.Var6;
-
-r1 = str2double(r1);
-r2 = str2double(r2);
 
 r1(1) = []; r2(1) = []; 
 
@@ -44,15 +38,15 @@ r1(1) = []; r2(1) = [];
 y = [y1,y2];
 s = [s1,s2];
 ref_point = [r1,r2];
-hvc = zeros(size(y,1),1); hvc_me = zeros(size(y,1),1); 
+hvc = zeros(size(y,1),1); 
+% hvc_me = zeros(size(y,1),1); 
 for i = 1:size(y,1)
-
         hvc(i,:) = EHVI_2d(PF,ref_point(i,:),y(i,:),s(i,:));
 %         hvc_me(i,:)=EHVI_monte_carlo(PF,ref_point(i,:),y(i,:),s(i,:));
 
 end
 
-hvc_me = csvread('hvc_mc_dtlz2_2d.csv');
+hvc_me = csvread('hvc_mc_dtlz2_2d.csv'); % already estimated
 min_hv = min([hvc;hvc_me]);
 max_hv = max([hvc;hvc_me]);
 
@@ -61,6 +55,6 @@ scatter(hvc,hvc_me);
 hold on;
 plot([min_hv,max_hv],[min_hv,max_hv])
 hold off;
-xlabel('hv contribution - Tinkle implementation')
-ylabel('hv contribution - MC')
+xlabel('EHVI - Analytical')
+ylabel('EHVI - MC')
 
